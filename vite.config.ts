f909ae5628903d3b5c21ed034d9ca405181367a3
Ayush Singh <ayushinsingh7@gmail.com>
2026-07-09 17:09:11 +0530

    changed base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "task-flow",
+  base: "taskflow",
   plugins: [react(), tailwindcss()],
   test: {
     globals: true,
